a6b9d45d1309c1d781488bee5d9ab78bef9a859f
Nicola Corti <corti.nico@gmail.com>
2022-12-01 17:56:10 +0100

    Solve Day 1

--- src/com/ncorti/aoc2022/Day01.kt
+++ src/com/ncorti/aoc2022/Day01.kt
@@ -1,14 +1,17 @@
 package com.ncorti.aoc2022
 
 fun main() {
-    fun part1(): Int =
-        getInputAsText("01") { split("\n") }
-            .count()
+  fun part1() = getInputAsText("01") { split("\n\n").map(String::trim) }
+    .maxOfOrNull {
+      it.split("\n").sumOf(String::toInt)
+    }
 
-    fun part2(): Int =
-        getInputAsText("01") { split("\n") }
-            .count()
+  fun part2() = getInputAsText("01") { split("\n\n").map(String::trim) }
+    .map { it.split("\n").sumOf(String::toInt) }
+    .sortedDescending()
+    .take(3)
+    .sum()
 
-    println(part1())
-    println(part2())
+  println(part1())
+  println(part2())
 }
